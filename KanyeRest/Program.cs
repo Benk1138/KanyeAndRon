a97650ce6b5792cf3d2a12af598eab4a54f81d42
benjamin kunkel <benjamink1125@gmail.com>
2022-11-29 07:23:14 -0800

    initial commit

---
+++ KanyeRest/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using Newtonsoft.Json.Linq;
+
+var client = new HttpClient();
+
+var kanyeURL = "https://api.kanye.rest/";
+
+var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+Console.WriteLine($"Kanye Says : \"{kanyeQuote}\"\n");
+
+
+var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+var ronResponse = client.GetStringAsync(ronURL).Result;
+
+var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+Console.WriteLine($"Ron Says: {ronQuote}\n");
